6fa7e915d3e95cbd4148e19e9198b1c9240280c0
AbhijeetDotexe <abhijeet444rana@gmail.com>
2024-08-04 19:55:43 +0530

    Removed unwanted code from the javascript file

--- public/script.js
+++ public/script.js
@@ -1,40 +1,25 @@
-const stripe = Stripe('pk_test_51BTUDGJAJfZb9HEBwDg86TN1KNprHjkfipXmEDMb0gSCassK5T3ZfxsAbcgKVmAIXF7oZ6ItlZZbXO6idTHE67IM007EwQ4uN3');
-// Use the form that already exists on the web page.
-const paymentMethodForm = document.getElementById('payment-method-form');
-const confirmationForm = document.getElementById('confirmation-form');
-
-paymentMethodForm.addEventListener('submit', (ev) => {
-  ev.preventDefault();
-  const accountHolderNameField = document.getElementById('account-holder-name-field');
-  const emailField = document.getElementById('email-field');
-
-  // Calling this method will open the instant verification dialog.
-  stripe.collectBankAccountForSetup({
-    clientSecret: clientSecret,
-    params: {
-      payment_method_type: 'us_bank_account',
-      payment_method_data: {
-        billing_details: {
-          name: accountHolderNameField.value,
-          email: emailField.value,
-        },
-      },
-    },
-    expand: ['payment_method'],
-  })
-  .then(({setupIntent, error}) => {
-    if (error) {
-      console.error(error.message);
-      // PaymentMethod collection failed for some reason.
-    } else if (setupIntent.status === 'requires_payment_method') {
-      // Customer canceled the hosted verification modal. Present them with other
-      // payment method type options.
-    } else if (setupIntent.status === 'requires_confirmation') {
-      // We collected an account - possibly instantly verified, but possibly
-      // manually-entered. Display payment method details and mandate text
-      // to the customer and confirm the intent once they accept
-      // the mandate.
-      confirmationForm.show();
-    }
+const processPayment = async function () {
+  let response = await fetch("http://localhost:3001/");
+  console.log("response", response);
+  if (!response.ok) {
+    console.log("Failed to get secret key");
+    return;
+  }
+  response = await response.json();
+  console.log("response", response);
+  const stripe = Stripe(
+    "testing"
+  );
+  const appearance = {
+    /* appearance */
+  };
+  const options = {
+    /* options */
+  };
+  const elements = stripe.elements({
+    clientSecret: response.clientSecret,
+    appearance,
   });
-});
+  const paymentElement = elements.create("payment", options);
+  paymentElement.mount("#payment-element");
+};
